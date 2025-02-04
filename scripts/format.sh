7224f72944f31c43cddfe6938d6b72742c1a173b
Bitca <support@projectbit.ca>
2025-02-04 14:17:46 -0300

    Update format.sh

--- scripts/format.sh
+++ scripts/format.sh
@@ -7,16 +7,16 @@
 
 CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 REPO_ROOT="$(dirname "${CURR_DIR}")"
-AGNO_DIR="${REPO_ROOT}/libs/agno"
-AGNO_DOCKER_DIR="${REPO_ROOT}/libs/infra/agno_docker"
-AGNO_AWS_DIR="${REPO_ROOT}/libs/infra/agno_aws"
+bitca_DIR="${REPO_ROOT}/libs/bitca"
+bitca_DOCKER_DIR="${REPO_ROOT}/libs/infra/bitca_docker"
+bitca_AWS_DIR="${REPO_ROOT}/libs/infra/bitca_aws"
 COOKBOOK_DIR="${REPO_ROOT}/cookbook"
 source ${CURR_DIR}/_utils.sh
 
 print_heading "Formatting all libraries"
-source ${AGNO_DIR}/scripts/format.sh
-source ${AGNO_DOCKER_DIR}/scripts/format.sh
-source ${AGNO_AWS_DIR}/scripts/format.sh
+source ${bitca_DIR}/scripts/format.sh
+source ${bitca_DOCKER_DIR}/scripts/format.sh
+source ${bitca_AWS_DIR}/scripts/format.sh
 
 # Format all cookbook examples
 source ${COOKBOOK_DIR}/scripts/format.sh
